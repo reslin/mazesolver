c127b127c6f407e361908d637231ceaf4fb4a22e
reslin <uwe.ploss@gmail.com>
2024-05-19 23:46:36 +0200

    add draw_move

--- main.py
+++ main.py
@@ -3,27 +3,32 @@ from cell import Cell
 
 def main():
     win = Window(800, 600)
-    
-    cells = [Cell(100, 200, 100, 200, win),
-             Cell(200, 300, 200, 300, win),
-             Cell(300, 400, 300, 400, win),
-             Cell(400, 500, 400, 500, win)]
 
-    for i in range(len(cells)):
-        if i % 2 == 0:
-            cells[i].has_bottom_wall = False
-        else:
-            cells[i].has_left_wall = False
-        cells[i].draw_cell()
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
+
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
+
+    c1.draw_move(c2)
+
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
 
     win.wait_for_close()
 
 
 main()
-
-
-
-# p1 = Point(100, 100)
-#     p2 = Point(700, 500)
-#     line = Line(p1, p2)
-#     win.draw_line(line, "black")
