dab685caef53c00ce39f707aaaaccefa745f665e
reslin <uwe.ploss@gmail.com>
2024-05-19 21:52:56 +0200

    add Point, Line and draw methods

--- graphics.py
+++ graphics.py
@@ -17,6 +17,29 @@ class Window:
         self.__running = True
         while self.__running:
             self.redraw()
+        print("finished...")
 
     def close(self):
-            self.__running = False
+        self.__running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color):
+        x1 = self.p1.x
+        y1 = self.p1.y
+        x2 = self.p2.x
+        y2 = self.p2.y
+        canvas.create_line(x1, y1, x2, y2, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from graphics import *
 
 def main():
     win = Window(800, 600)
+    p1 = Point(100, 100)
+    p2 = Point(700, 500)
+    line = Line(p1, p2)
+    win.draw_line(line, "black")
+    p1 = Point(700, 100)
+    p2 = Point(100, 500)
+    line = Line(p1, p2)
+    win.draw_line(line, "blue")
     win.wait_for_close()
 
 
