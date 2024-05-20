734c5da7679b06103fe185fa264ee60409aa1693
reslin <uwe.ploss@gmail.com>
2024-05-20 12:07:13 +0200

    simplify cell center coords calculation

--- cell.py
+++ cell.py
@@ -33,13 +33,11 @@ class Cell:
             self._win.draw_line(line)
 
     def draw_move(self, to_cell, undo=False):
-        half_length = abs(self._x2 - self._x1) // 2
-        x_center = half_length + self._x1
-        y_center = half_length + self._y1
+        x_center = (self._x1 + self._x2) // 2
+        y_center = (self._y1 + self._y2) // 2
 
-        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
-        x_center2 = half_length2 + to_cell._x1
-        y_center2 = half_length2 + to_cell._y1
+        x_center2 = (to_cell._x1 + to_cell._x2) // 2
+        y_center2 = (to_cell._y1 + to_cell._y2) // 2
 
         fill_color = "red"
         if undo:
